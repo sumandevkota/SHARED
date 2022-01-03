31087988abe875e9b3bfe87f7f32a6c570b0a593
Bhishan Poudel <bhishanpdl@users.noreply.github.com>
2022-01-02 20:38:55 -0500

    Add files via upload

--- parse_keyword_planner_google.py
+++ parse_keyword_planner_google.py
@@ -14,14 +14,13 @@ with open('key.csv','r',encoding='utf-16') as fi:
 names = [i.strip() for i in names]
 print(names)
 
-
+# Read local file key.csv
 df = pd.read_csv('key.csv',skiprows=1,nrows=None,
                 encoding='utf-16',delimiter='\t',header=None,names=names)
-
 print(df.shape)
 print(df.head(2).append(df.tail(2)))
 
-# Read another file
+# Read file from github
 url = "https://raw.githubusercontent.com/sumandevkota/SHARED/main/Tshirt.csv"
 
 df = pd.read_csv(url,skiprows=1,nrows=None,
